738f7459ae9a27a425fdd75742d944dc03bd19fa
Koen Veldhorst <koen.veldhorst@gmail.com>
2025-06-18 12:32:45 +0200

    modified config

--- src/config.py
+++ src/config.py
@@ -7,3 +7,5 @@ GENERATED_IMAGE_SIZE = (512, 512)
 # path configuration
 ROOT_DIR = Path(__file__).parent.parent
 DATASET_DIR = os.path.join(ROOT_DIR, 'dataset')
+
+RUNTIME_CONFIG_PATH = os.path.join(ROOT_DIR, 'src/config.json')
